496f07497fdd73ebed6a303db605e97702de1276
Olli Petäjäniemi <petajaol@gmail.com>
2023-11-04 01:03:44 +0200

    rename functions, add sending own message back to user

--- websocket_server.py
+++ websocket_server.py
@@ -8,30 +8,26 @@ message_deque = deque(maxlen=message_deque_max_length)
 
 
 async def handle_connection(websocket, path):
-    # Add the websocket to a list of connected clients
     user_name = await ask_user_name(websocket)
-    await send_message_to_all_except_one(user_name, "'{}' joined. Please be kind to them.".format(user_name))
+    message = f"{user_name} joined. Please be kind to them."
+    await broadcast_message(message)
     connected_clients[user_name] = websocket
     await websocket.send("Connected!")
     if len(message_deque) > 0:
         await websocket.send(create_string_from_message_deque())
     try:
         while True:
-            # Receive a message from the client
             message = await websocket.recv()
             message_deque.append("{}: {}".format(user_name, message))
 
-            # Broadcast the message to all connected clients
-            await send_message_to_all_except_one(user_name, "{}: {}".format(user_name, message))
+            await broadcast_message(message)
     finally:
-        # Remove the websocket from the list of connected clients
         del connected_clients[user_name]
 
 
-async def send_message_to_all_except_one(user_name, message):
+async def broadcast_message(message):
     for key in connected_clients.keys():
-        if key != user_name:
-            await connected_clients[key].send(message)
+        await connected_clients[key].send(message)
 
 
 def create_string_from_message_deque():
@@ -52,8 +48,7 @@ async def ask_user_name(client):
         else:
             return user_name
 
-# Start the WebSocket server
+
 start_server = websockets.serve(handle_connection, "localhost", 8000)
-# Run the server indefinitely
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
