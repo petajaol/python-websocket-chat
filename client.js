1c624c6f53a64c4fc6556efad50d956123f08e5c
Olli Petäjäniemi <petajaol@gmail.com>
2023-11-05 00:42:32 +0200

    feat: show connected users in a sidebar

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const chatBox = document.getElementById("chat-box");
+const userList = document.getElementById("user-list");
 
 const ws = new WebSocket("ws://localhost:8000");
 
 window.addEventListener("beforeunload", () => {
   chatBox.value = "";
+  userList.value = "";
 });
 
 ws.addEventListener("message", (event) => {
@@ -12,21 +14,60 @@ ws.addEventListener("message", (event) => {
 
 function handleIncomingMessages(event) {
   try {
-    parsedData = JSON.parse(event.data);
-    if (Array.isArray(parsedData)) {
-      parsedData.forEach((message) => {
-        chatBox.value += "\n" + parseMessage(message);
+    const parsedData = JSON.parse(event.data);
+
+    const handleJoinMessage = () => {
+      chatBox.value += "\n" + parsedData.message;
+      userList.value += "\n" + parsedData.user_name;
+    };
+
+    const handleLeaveMessage = () => {
+      chatBox.value += "\n" + parsedData.message;
+      const userToRemove = parsedData.user_name;
+      const userListArray = userList.value.split('\n');
+      const userIndex = userListArray.indexOf(userToRemove);
+      if (userIndex !== -1) {
+        userListArray.splice(userIndex, 1);
+        userList.value = userListArray.join('\n');
+      };
+    };
+
+    const handleChatMessage = () => {
+      chatBox.value += "\n" + parseChatMessage(parsedData);
+    };
+
+    const handleMessageLog = () => {
+      parsedData.messages.forEach((message) => {
+        chatBox.value += "\n" + parseChatMessage(message);
       });
-    } else {
-      chatBox.value += "\n" + parseMessage(parsedData);
+    };
+
+    const handleUserList = () => {
+      parsedData.users.forEach((user) => {
+        userList.value += "\n" + user;
+      });
+    };
+
+    const messageHandlers = {
+      join_message: handleJoinMessage,
+      part_message: handleLeaveMessage,
+      chat_message: handleChatMessage,
+      message_log: handleMessageLog,
+      user_list: handleUserList,
+    };
+
+    const messageType = parsedData.type;
+    if (messageHandlers[messageType]) {
+      messageHandlers[messageType]();
     }
   } catch (error) {
     chatBox.value += "\n" + event.data;
   }
+
   chatBox.scrollTop = chatBox.scrollHeight
 }
 
-function parseMessage(message) {
+function parseChatMessage(message) {
   return `[${message.timestamp}] ${message.user_name}: ${message.message} `
 }
 
--- websocket_server.py
+++ websocket_server.py
@@ -3,6 +3,7 @@ import json
 from collections import deque
 from datetime import datetime
 import websockets
+import pytz
 
 
 connected_clients = {}
@@ -13,42 +14,63 @@ message_log = deque(maxlen=MESSAGE_LOG_MAX_LENGTH)
 async def initialize_connection(client, path):
     user_name = await ask_user_name(client)
     connected_clients[user_name] = client
-    await broadcast_message(f"{user_name} joined. Please be kind to them.", user_name)
+    message = {
+        "type": "join_message",
+        "user_name": user_name,
+        "message": f"{user_name} joined. Please be kind to them.",
+    }
+    await broadcast_message(message)
     await retrieve_message_log(client)
-    await client.send("Connected!")
+    await retrieve_connected_users(client)
+    await client.send(f"Joined as {user_name}!")
     while True:
         try:
             await wait_for_messages(user_name, client)
         except websockets.exceptions.ConnectionClosed:
             if user_name in connected_clients:
-                await broadcast_message(f"{user_name} disconnected.", user_name)
+                message = {
+                    "type": "part_message",
+                    "user_name": user_name,
+                    "message": f"{user_name} left.",
+                }
+                await broadcast_message(message)
                 del connected_clients[user_name]
             break
 
 
 async def wait_for_messages(user_name, client):
-    while True:
-        message = {
-            "user_name": user_name,
-            "message": await client.recv(),
-            "timestamp": datetime.now().strftime("%H:%M:%S"),
-        }
-        message_log.append(message)
-        await broadcast_message(json.dumps(message))
+    message = {
+        "type": "chat_message",
+        "user_name": user_name,
+        "message": await client.recv(),
+        "timestamp": datetime.now(pytz.timezone("Europe/Helsinki")).strftime(
+            "%H:%M:%S"
+        ),
+    }
+    message_log.append(message)
+    await broadcast_message(message)
+
+
+async def retrieve_connected_users(client):
+    if len(connected_clients) > 0:
+        users_json = json.dumps(
+            {"type": "user_list", "users": list(connected_clients.keys())}
+        )
+        await client.send(users_json)
 
 
 async def retrieve_message_log(client):
     if len(message_log) > 0:
-        message_json = json.dumps(list(message_log))
+        message_json = json.dumps(
+            {"type": "message_log", "messages": list(message_log)}
+        )
         await client.send(message_json)
 
 
-async def broadcast_message(message, sender_name=None):
+async def broadcast_message(message):
     for user_name, client in connected_clients.items():
-        if sender_name is None or (
-            sender_name is not None and user_name != sender_name
-        ):
-            await client.send(message)
+        if message["type"] == "chat_message" or user_name != message["user_name"]:
+            await client.send(json.dumps(message))
 
 
 async def ask_user_name(client):
